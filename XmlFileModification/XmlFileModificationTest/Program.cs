6eef1a85b438796201c08003aa09fce9c0b32170
DESKTOP-I1C8DIN\gnute <gnutekandrzej@gmail.com>
2022-05-23 23:26:05 +0200

    Xml read/write test

--- XmlFileModification/XmlFileModificationTest/Program.cs
+++ XmlFileModification/XmlFileModificationTest/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using XmlFileModificationTest;
+
 Console.WriteLine("Hello, World!");
+
+
+var pathFile = "C:\\app\\XmlFileReadWrite\\XmlFileModification\\XmlFileModificationTest\\XmlFile\\file.xml";
+
+var xmlFileTest = new XmlFileMod();
+xmlFileTest.ReadDataXmlFile(pathFile);
---
+++ XmlFileModification/XmlFileModificationTest/XmlFileMod.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml;
+
+namespace XmlFileModificationTest
+{
+    public class XmlFileMod
+    {
+        public void ReadDataXmlFile(string pathToFile)
+        {
+
+            XmlTextReader textReader = new XmlTextReader("C:\\app\\XmlFileReadWrite\\XmlFileModification\\XmlFileModificationTest\\XmlFile\\file.xml");
+            //string test = "aaa";
+            //string test1 = "";
+            //while (textReader.Read())
+            //{
+            //    //Console.WriteLine(textReader.Name);
+            //    //textReader.MoveToNextAttribute();
+            //    //Console.WriteLine(textReader.Value);
+
+            //    if (test1 == "Challenge")
+            //    {
+            //        test = textReader.Value;
+            //        test1 = null;
+            //    }
+
+
+            //    if (textReader.Name == "Challenge" && test1 != null)
+            //    {
+
+            //        test1 = textReader.Name;
+
+            //    }
+
+
+            //}
+
+            //Console.WriteLine(test);
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("C:\\app\\XmlFileReadWrite\\XmlFileModification\\XmlFileModificationTest\\XmlFile\\file.xml");
+
+            //XmlNode xmlNode = xmlDocument.DocumentElement.GetElementsByTagName("Challenge").Item(0);
+            
+            //Console.WriteLine(xmlNode.FirstChild.Value);
+
+            var a = xmlDocument.GetElementsByTagName("Challenge").Item(0);
+            //if(a?.FirstChild != null)
+                Console.WriteLine(a?.FirstChild?.Value);
+
+            a.FirstChild.Value = "asasasass";
+            xmlDocument.Save("C:\\app\\XmlFileReadWrite\\XmlFileModification\\XmlFileModificationTest\\XmlFile\\file.xml");
+
+
+            var aa = xmlDocument.GetElementsByTagName("Identifier").Item(0);
+
+            Console.WriteLine(aa.FirstChild.FirstChild.Value);
+        }
+    }
+}
